50c84baa6ec9c5ede4aba889569a7c61a63d75dd
Jeremy Parker <github@jeremyparker.org>
2018-03-08 16:11:09 -0500

    Add Route And Controller For Plan

---
+++ app/controllers/plans_controller.rb
@@ -0,0 +1,46 @@
+include Response
+
+class PlansController < ApplicationController
+
+  before_action :set_plan, only: [:show, :update, :destroy]
+
+  # GET /plans
+  def index
+    @plans = Plan.all
+    json_response(@plans)
+  end
+
+  # POST /plans
+  def create
+    @plan = Plan.create!(plan_params)
+    json_response(@plan, :created)
+  end
+
+  # GET /plans/:id
+  def show
+    json_response(@plan)
+  end
+
+  # PUT /plans/:id
+  def update
+    @plan.update(plan_params)
+    head :no_content
+  end
+
+  # DELETE /plans/:id
+  def destroy
+    @plan.destroy
+    head :no_content
+  end
+
+  private
+
+  def plan_params
+    # whitelist params
+    params.permit(:title, :created_by_slack_user)
+  end
+
+  def set_plan
+    @plan = Plan.find(params[:id])
+  end
+end
