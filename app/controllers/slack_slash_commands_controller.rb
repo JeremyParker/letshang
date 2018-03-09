721189853bd7b30d5f3a65cf6fbbe633a4347a45
Jeremy Parker <github@jeremyparker.org>
2018-03-08 21:43:13 -0500

    Attempting To Authenticate And Call Web API

--- app/controllers/slack_slash_commands_controller.rb
+++ app/controllers/slack_slash_commands_controller.rb
@@ -1,5 +1,6 @@
 include Response
 include SlackToken
+require 'slack-ruby-client'
 
 # Handles all requests from Slack that come in the form of a slash command
 class SlackSlashCommandsController < ApplicationController
@@ -9,6 +10,14 @@ class SlackSlashCommandsController < ApplicationController
     puts request
     return json_response({}, status: 403) unless valid_slack_token?
     json_response({cool: 'cool'}, :created)
-  end
 
+    Slack.configure do |config|
+      config.token = ENV['SLACK_APP_TOKEN']
+      raise 'Missing ENV[SLACK_APP_TOKEN]!' unless config.token
+    end
+
+    client = Slack::Web::Client.new
+    client.auth_test
+    client.chat_postMessage(channel: '#general', text: 'Hello World', as_user: true)
+  end
 end
