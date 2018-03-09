f6bdd0cd2b758298f75f29082470ef51fc90785f
Jeremy Parker <github@jeremyparker.org>
2018-03-09 09:41:39 -0500

    Working On Handling Initial Submission

--- app/controllers/slack_submissions_controller.rb
+++ app/controllers/slack_submissions_controller.rb
@@ -6,7 +6,12 @@ class SlackSubmissionsController < ApplicationController
   # POST /slack_submission
   def create
     puts request
+    if params[:payload][:type] == 'interactive_message'
+      if params[:payload][:callback_id] == 'create_plan'
+        json_response('Yay! Starting a plan')
+      end
+    end
 
-    json_response({cool: 'cool'}, :created)
+    json_response('Woah, something weird happened')
   end
 end
