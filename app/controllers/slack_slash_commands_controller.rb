c6a4d7e134928a3829c805e57bbb8e55d7f9ae04
Jeremy Parker <github@jeremyparker.org>
2018-03-09 01:04:27 -0500

    Try Responding With Buttons

--- app/controllers/slack_slash_commands_controller.rb
+++ app/controllers/slack_slash_commands_controller.rb
@@ -9,18 +9,56 @@ class SlackSlashCommandsController < ApplicationController
   def create
     puts request
     return json_response({}, status: 403) unless valid_slack_token?
-    json_response({message: 'cool'}, :created)
+    json_response({
+      "text": "Would you like to play a game?",
+      "attachments": [
+        {
+          "text": "Choose a game to play",
+          "fallback": "You are unable to choose a game",
+          "callback_id": "wopr_game",
+          "color": "#3AA3E3",
+          "attachment_type": "default",
+          "actions": [
+            {
+              "name": "game",
+              "text": "Chess",
+              "type": "button",
+              "value": "chess"
+            },
+            {
+              "name": "game",
+              "text": "Falken's Maze",
+              "type": "button",
+              "value": "maze"
+            },
+            {
+              "name": "game",
+              "text": "Thermonuclear War",
+              "style": "danger",
+              "type": "button",
+              "value": "war",
+              "confirm": {
+                  "title": "Are you sure?",
+                  "text": "Wouldn't you prefer a good game of chess?",
+                  "ok_text": "Yes",
+                  "dismiss_text": "No"
+              }
+            }
+          ]
+        }
+      ]
+    }, :created)
 
-    Slack.configure do |config|
-      config.token = ENV['SLACK_BOT_USER_TOKEN']
-      unless config.token
-        Rails.logger.debug 'Missing ENV[SLACK_BOT_USER_TOKEN]!'
-        raise 'Missing ENV[SLACK_BOT_USER_TOKEN]!'
-      end
-    end
+    # Slack.configure do |config|
+    #   config.token = ENV['SLACK_BOT_USER_TOKEN']
+    #   unless config.token
+    #     Rails.logger.debug 'Missing ENV[SLACK_BOT_USER_TOKEN]!'
+    #     raise 'Missing ENV[SLACK_BOT_USER_TOKEN]!'
+    #   end
+    # end
 
-    client = Slack::Web::Client.new
-    Rails.logger.debug client.auth_test
-    client.chat_postMessage(channel: params[:channel_id], text: 'Hello World', as_user: false)
+    # client = Slack::Web::Client.new
+    # Rails.logger.debug client.auth_test
+    # client.chat_postMessage(channel: params[:channel_id], text: 'Hello World', as_user: false)
   end
 end
