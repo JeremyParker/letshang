b20c89355af7fbaeb865e853ae54b35f258f0590
Jeremy Parker <github@jeremyparker.org>
2018-03-09 01:09:37 -0500

    Set Text And Options For Intro

--- app/controllers/slack_slash_commands_controller.rb
+++ app/controllers/slack_slash_commands_controller.rb
@@ -5,44 +5,38 @@ require 'slack-ruby-client'
 # Handles all requests from Slack that come in the form of a slash command
 class SlackSlashCommandsController < ApplicationController
 
+  INTRO_MESSAGE = 'Hi there, I’m here to help you effortlessly plan a shindig! \
+You pick the time, attendees, minimum number of people and 2-8 options of what \
+you’d like to do. I’ll send the request and collect the votes. I can get the \
+minimum number of people agree to on an option within 2 hours, I’ll create a \
+group chat and announce the winning option. If a minimum number of attendees is \
+not reached, I’ll cancel the request.'
+
   # POST /slack_slash_command
   def create
     puts request
     return json_response({}, status: 403) unless valid_slack_token?
     json_response({
-      "text": "Would you like to play a game?",
+      "text": INTRO_MESSAGE,
       "attachments": [
         {
-          "text": "Choose a game to play",
-          "fallback": "You are unable to choose a game",
-          "callback_id": "wopr_game",
+          "text": "Sound good?",
+          "fallback": "You are unable to create a plan",
+          "callback_id": "create_plan",
           "color": "#3AA3E3",
           "attachment_type": "default",
           "actions": [
             {
-              "name": "game",
-              "text": "Chess",
+              "name": "response",
+              "text": "Yeah, let's make a plan",
               "type": "button",
-              "value": "chess"
+              "value": "yes"
             },
             {
-              "name": "game",
-              "text": "Falken's Maze",
+              "name": "response",
+              "text": "Nah, I'm good",
               "type": "button",
-              "value": "maze"
-            },
-            {
-              "name": "game",
-              "text": "Thermonuclear War",
-              "style": "danger",
-              "type": "button",
-              "value": "war",
-              "confirm": {
-                  "title": "Are you sure?",
-                  "text": "Wouldn't you prefer a good game of chess?",
-                  "ok_text": "Yes",
-                  "dismiss_text": "No"
-              }
+              "value": "no"
             }
           ]
         }
