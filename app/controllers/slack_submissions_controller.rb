04ce259783fc29b39752147729de0c8ad11cd34f
Jeremy Parker <github@jeremyparker.org>
2018-03-09 09:33:52 -0500

    Remove Token Check From Submit Controller

--- app/controllers/slack_submissions_controller.rb
+++ app/controllers/slack_submissions_controller.rb
@@ -6,7 +6,7 @@ class SlackSubmissionsController < ApplicationController
   # POST /slack_submission
   def create
     puts request
-    return json_response({}, status: 403) unless valid_slack_token?
+
     json_response({cool: 'cool'}, :created)
   end
 end
