ae0133504e9d961905e8c0907835c138762e03f4
Jeremy Parker <github@jeremyparker.org>
2018-03-11 11:55:31 -0400

    Authorizing From Install Button Creates Team Object

--- app/controllers/auth_redirects_controller.rb
+++ app/controllers/auth_redirects_controller.rb
@@ -23,33 +23,35 @@ class AuthRedirectsController < ApplicationController
       response = client.oauth_access(options)
 
       if response[:ok]
-        access_token = response[:access_token]
-        scope = response[:scope]
-        user_id = response[:user_id] # flavri is 'U02CWFEEJ'
-        team_name = response[:team_name]
-        team_id = response[:team_id]
-        bot_user_id = response[:bot][:bot_user_id]
-        bot_access_token = response[:bot][:bot_access_token]
-
-        # testing
-        Slack.configure do |config|
-          config.token = bot_access_token
-          config.logger = Rails::logger
-        end
-        client = Slack::Web::Client.new
-
-        # testing what we can do with the different tokens
-        require 'pry'; binding.pry
-        response = client.conversations_open({return_im: true, users: user_id})
-        require 'pry'; binding.pry
-        client.chat_postMessage(
-          channel: response[:channel][:id],
-          text: "Thanks for installing Let's Hang!",
-          as_user: false
-        )
-
         # create or update Team object, and store relevant tokens
+        new_team_data = response.to_hash.symbolize_keys
+        new_team_data.delete(:ok) # don't need the :ok value
 
+        # flatten the 'bot' hash
+        new_team_data[:bot_user_id] = response[:bot][:bot_access_token]
+        new_team_data[:bot_access_token] = response[:bot][:bot_access_token]
+        new_team_data.delete(:bot)
+
+        Team.create(new_team_data)
+
+        render body: "Hi #{new_team_data[:team_name]}! Thanks for installing \"Let's Hang\"!", status: status
+
+        # # testing
+        # Slack.configure do |config|
+        #   config.token = bot_access_token
+        #   config.logger = Rails::logger
+        # end
+        # client = Slack::Web::Client.new
+
+        # # testing what we can do with the different tokens
+        # require 'pry'; binding.pry
+        # response = client.conversations_open({return_im: true, users: user_id})
+        # require 'pry'; binding.pry
+        # client.chat_postMessage(
+        #   channel: response[:channel][:id],
+        #   text: "Thanks for installing Let's Hang!",
+        #   as_user: false
+        # )
       else
         # redirect to some error page?
       end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180308201601) do
+ActiveRecord::Schema.define(version: 20180311141543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,18 @@ ActiveRecord::Schema.define(version: 20180308201601) do
   create_table "plans", force: :cascade do |t|
     t.string "title"
     t.string "created_by_slack_user"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "access_token"
+    t.string "scope"
+    t.string "user_id"
+    t.string "team_name"
+    t.string "team_id"
+    t.string "bot_user_id"
+    t.string "bot_access_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
