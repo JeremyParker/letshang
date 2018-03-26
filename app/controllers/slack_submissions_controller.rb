774b6cec55fdf834cad3a56cfb0ca9bd931d3568
Jeremy Parker <github@jeremyparker.org>
2018-03-26 10:38:40 -0400

    User Is Asked For Min Group Size And Rough Time

--- app/controllers/slack_submissions_controller.rb
+++ app/controllers/slack_submissions_controller.rb
@@ -4,81 +4,105 @@ include SlackToken
 class SlackSubmissionsController < ApplicationController
   protect_from_forgery :except => [:create] # we check the token 'manually' with `valid_slack_token`
 
-  TEST_DIALOG = {
-    "callback_id": "ryde-46e2b0",
-    "title": "Request a Ride",
-    "submit_label": "Request",
-    "elements": [
-      {
-        "type": "text",
-        "label": "Pickup Location",
-        "name": "loc_origin"
-      },
-      {
-        "type": "text",
-        "label": "Dropoff Location",
-        "name": "loc_destination"
-      }
-    ]
-  }
-
   # POST /slack_submission
+  # This method expects a payload from Slack like this:
+  # {
+  #   "type"=>"dialog_submission",
+  #   "submission"=>{"plan_size"=>"2"},
+  #   "callback_id"=>"set_plan_size:18",
+  #   "team"=>{"id"=>"T02CVNHRP", "domain"=>"lollycom"},
+  #   "user"=>{"id"=>"U02CVNHRR", "name"=>"jeremy"},
+  #   "channel"=>{"id"=>"C9LPTG4E4", "name"=>"testing"},
+  #   "action_ts"=>"1522019535.632639",
+  #   "token"=>"<redacted>",
+  #   "response_url"=>"https://hooks.slack.com/app/T02CVNHRP/335745644371/ybOjTi2mV43CgnH4GVp1u9ox"
+  # }
   def create
     payload = JSON.parse params[:payload]
     return json_response({}, :forbidden) unless valid_slack_token? payload['token']
 
-    if payload['type'] == 'interactive_message'
+    case payload['type']
+
+    when 'dialog_submission'
       case payload['callback_id']
-        when 'start_plan_response'
-          team = Team.where(team_id: payload['team']['id']).order(:updated_at).last
-          trigger_id = payload['trigger_id']
+      when /^set_plan_size/
+        minimum_attendee_count = payload['submission']['plan_size'].to_i # TODO - error handling, bounds checking
+        plan = Plan.find(payload['callback_id'].split(':').last)
+        plan.update(minimum_attendee_count: minimum_attendee_count)
 
-          # TODO: how do we match this with the plan we created in the previous step?
+        Slack.configure do |config|
+          config.token = plan.owner.team.bot_access_token
+          config.logger = Rails::logger
+        end
+        client = Slack::Web::Client.new
+        client.chat_postEphemeral(
+          channel: payload['channel']['id'],
+          user: payload['user']['id'],
+          text: "",
+          attachments: [
+            {
+              "callback_id": "plan_time:#{plan.id}",
+              "attachment_type": "default",
+              "color": "#3AA3E3",
+              "text": "OK, we're planning an outing of at least #{minimum_attendee_count} people. When do you want this to happen?",
+              "actions": [
+                {
+                  "name": "plan_time",
+                  "text": "Pick one",
+                  "type": "select",
+                  "options": [
+                    {
+                        "text": "Tonight",
+                        "value": "today"
+                    },
+                    {
+                        "text": "Tomorrow",
+                        "value": "tomorrow"
+                    },
+                    {
+                        "text": "After tomorrow",
+                        "value": "later"
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        )
+        json_response({}, :ok)
+    end
 
-          Slack.configure do |config|
-            config.token = team.bot_access_token
-            config.logger = Rails::logger
-          end
-          client = Slack::Web::Client.new
-          response =client.dialog_open(token: team.bot_access_token, dialog: TEST_DIALOG, trigger_id: trigger_id)
-
-          json_response({}, :created)
-
-          # response = client.conversations_open({return_im: true, users: user_id})
-          # channel = payload['channel']['id']
-          # client.chat_postMessage(
-          #   channel: response[:channel][:id],
-          #   text: "Thanks for installing Let's Hang!",
-          #   as_user: false
-          # )
-
-          # # A response an include can interactive message with buttons
-          # json_response({
-          #   "text": 'Testing buttons in a response',
-          #   "attachments": [
-          #     {
-          #       "text": "Sound good?",
-          #       "fallback": "You are unable to create a plan",
-          #       "callback_id": "create_plan",
-          #       "color": "#3AA3E3",
-          #       "attachment_type": "default",
-          #       "actions": [
-          #         {
-          #           "name": "response",
-          #           "text": "Yeah, let's make a plan",
-          #           "type": "button",
-          #           "value": "yes"
-          #         },
-          #         {
-          #           "name": "response",
-          #           "text": "Nah, I'm good",
-          #           "type": "button",
-          #           "value": "no"
-          #         }
-          #       ]
-          #     }
-          #   ]
-          # }, :created)
+    # An interactive message expects the payload to look like
+    # {
+    #   "type"=>"interactive_message",
+    #   "actions"=>[{"name"=>"plan_time", "type"=>"select", "selected_options"=>[{"value"=>"today"}]}],
+    #   "callback_id"=>"plan_time:21",
+    #   "team"=>{"id"=>"T02CVNHRP", "domain"=>"lollycom"},
+    #   "channel"=>{"id"=>"C9LPTG4E4", "name"=>"testing"},
+    #   "user"=>{"id"=>"U02CVNHRR", "name"=>"jeremy"},
+    #   "action_ts"=>"1522035190.124049",
+    #   "message_ts"=>"1522035178.000066",
+    #   "attachment_id"=>"1",
+    #   "token"=>"<redacted>",
+    #   "is_app_unfurl"=>false,
+    #   "response_url"=>"https://hooks.slack.com/actions/T02CVNHRP/335796550691/WQOZmoeCQKZqxatQmSodkqcN",
+    #   "trigger_id"=>"336718987558.2437765873.d037056e1569134854d268751706725a"
+    # }
+    when 'interactive_message'
+      case payload['callback_id']
+      when /^plan_time/
+        plan = Plan.find(payload['callback_id'].split(':').last)
+        input = payload['actions'][0]['selected_options'][0]['value']
+        date = case input
+        when 'today'
+          Date.today
+        when 'tomorrow'
+          Date.tomorrow
+        else
+          nil #TODO some error message and show them the dialog again
+        end
+        plan.update(rough_time: date)
+        json_response({text: "Great. To add some options for people to do, enter `/letshang add`"}, :ok)
       else
         json_response("Uh oh! I don't know what callback that was for")
       end
