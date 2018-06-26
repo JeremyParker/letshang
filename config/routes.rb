5a5f67157ba8a9cc76f04e2e00b853e414c1d279
Jeremy Parker <github@jeremyparker.org>
2018-06-26 16:24:28 -0400

    Make The Slack Add Button ClientID Dynamic

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  get 'home/index'
+
+  get "/" => "home#index"
+
   resources :answers
   resources :teams
   resources :responses
